cddcca235c3733d77a66a489e681b696efec2614
younghwan22 <syh040717@naver.com>
2025-07-03 20:45:25 +0900

    메롱

---
+++ src/pages/Main/Main2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main2 = () => {
+    return (
+        <div className class="relative w-full min-h-screen pt-0 mt-0 bg-center bg-cover leading-none">
+            {/* // <div className=''></div>
+            // <p className=' pt-5 text-[22px] font-semibold'>성결대학교 멋쟁이사자처럼</p>
+            // <img src="images/main/mainP2.png" alt="" /> */}
+
+        </div>
+    );
+};
+
+export default Main2;
--- src/pages/Main/Mainall.jsx
+++ src/pages/Main/Mainall.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Main1 from './Main1';
+import Main2 from './Main2';
 
 const Mainall = () => {
-    return (
-        <Main1 />
-    );
+  return (
+    <>
+      <Main1 />
+      <Main2 />
+    </>
+  );
 };
 
 export default Mainall;
