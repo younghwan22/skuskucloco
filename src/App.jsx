9fad0375728b72cee939352287029615ba434a48
admin <ntk9477@naver.com>
2025-07-10 14:22:39 +0900

    TK

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
 import Mainall from './pages/Main/Mainall';
+import Project from "./pages/Project";
+import Team from "./pages/Team";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer'; // Footer 컴포넌트를 가져옵니다.
 
 function App() {
   return (
     <BrowserRouter>
+  
       <Routes>
         <Route path="/" element={<Mainall />} />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/Team" element={<Team />}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
+
+
